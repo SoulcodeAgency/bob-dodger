82aef17bc32ce3f6d0770e8b4f511e595059c004
Ernst Joss <ernst.joss@infocentric.ch>
2019-05-23 13:25:48 +0200

    provided package.json

--- generateDocs.ps1
+++ generateDocs.ps1
@@ -2,9 +2,17 @@
 
 $module = "Dodger"
 
-Import-Module "$PSScriptRoot\packages\Unic.Bob.Keith\Keith"
+Import-Module "$PSScriptRoot\packages\Unic.Bob.Keith\Keith" -Force
 Import-Module "$PSScriptRoot\src\$module" -Force
 
 New-PsDoc -Module $module -Path "$PSScriptRoot\docs\" -OutputLocation "$PSScriptRoot\docs-generated"
+
+if(-not (Test-Path "$PSScriptRoot\temp")) {
+    mkdir "$PSScriptRoot\temp"
+}
+
+Copy-Item .\package.json.default "$PSScriptRoot\temp\package.json"
+
+
 
 New-GitBook "$PSScriptRoot\docs-generated" "$PSScriptRoot\temp"
